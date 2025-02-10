9f8e7869d82ac55e0b9a766a122f9f17ea21e349
Tanya <50575800+TanyaZHu@users.noreply.github.com>
2025-02-10 16:09:34 +0100

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,14 +1,12 @@
 <?php
-$host = getenv('MYSQL_HOST') ?: 'mysql_db';
-$dbname = getenv('MYSQL_DATABASE') ?: 'mydatabase';
-$user = getenv('MYSQL_USER') ?: 'user';
-$pass = getenv('MYSQL_PASSWORD') ?: 'password';
+$dsn = "pgsql:host=monorail.proxy.rlwy.net;port=53676;dbname=railway";
+$user = "postgres";
+$password = "yBvUgaqtraIRooWftLCLkGtjCFzYuonj";
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Підключення до бази даних успішне!";
+    $pdo = new PDO($dsn, $user, $password, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+    echo "✅ Підключення успішне!";
 } catch (PDOException $e) {
-    die("Помилка підключення: " . $e->getMessage());
+    die("❌ Помилка підключення: " . $e->getMessage());
 }
 ?>
