6a632fdd9bd89e102091e65328cb6708a358cdea
Tetiana <50575800+TanyaZHu@users.noreply.github.com>
2025-02-15 16:50:56 +0100

    Update animal.js

--- js/animal.js
+++ js/animal.js
@@ -1,20 +1,16 @@
-﻿document.addEventListener("DOMContentLoaded", async function () {
+document.addEventListener("DOMContentLoaded", async function () {
     const urlParams = new URLSearchParams(window.location.search);
-    const animalId = urlParams.get("id") || 1; // Беремо ID тварини з URL
-
+    const animalId = urlParams.get("id") || 1;
 
     try {
         let response = await fetch(`http://localhost/api.php?animal_id=${animalId}`);
         let animalData = await response.json();
 
-
         document.getElementById("animal-name").textContent = animalData.name;
         document.getElementById("animal-age").textContent = animalData.age;
         document.getElementById("animal-gender").textContent = animalData.gender === "M" ? "Male" : "Female";
-        document.getElementById("animal-species").textContent = animalData.species;
         document.getElementById("animal-health").textContent = animalData.health_info;
         document.getElementById("animal-description").textContent = animalData.description;
-
 
         const gallery = document.getElementById("photo-gallery");
         gallery.innerHTML = "";
